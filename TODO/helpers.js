713437c3e3a12d77b15f7619fef6ff567ca529ce
MahaAmin <mahaamin97@gmail.com>
2020-05-10 22:47:59 +0200

    Finish editTodo

--- TODO/helpers.js
+++ TODO/helpers.js
@@ -23,16 +23,7 @@ function writeTodosToFile(filePath, todosJSONArray){
 
 
 exports.addTodo = function addTodo(options){
-    /*
-        1) Read all todos
-        2) convert todos from strings to JSON
-        3) get last id
-        4) create new todo
-        5) push the new todo to old todos
-        6) convert todos from json to strings
-        7) write todos to todosData file
-    */
-    
+
     const todosData = readTodosFromFile('todosData');
     
     var maxID = 0;
@@ -53,7 +44,19 @@ exports.addTodo = function addTodo(options){
 }
 
 exports.editTodo = function editTodo(options){
-    console.log("in editTodo");
+     
+    todosData = readTodosFromFile('todosData');
+    for(i=0; i<todosData.length; i++){
+        if(todosData[i]["id"] == options["id"]){
+            for(var key of Object.keys(options)){
+                if(key != 'command'){
+                    todosData[i][key] = options[key];
+                }
+            }
+            break;
+        }
+    } 
+    writeTodosToFile('todosData', todosData);
 }
 
 exports.listTodo = function listTodo(options){
